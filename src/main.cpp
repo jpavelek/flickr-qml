11e7bb86df8f976f0a3e9bac02fcd749621fe005
Jakub Pavelek <jpavelek@live.com>
2011-11-12 22:28:07 +0100

    Changes: Proper window title set

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,17 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 {
 #ifdef __arm__
     QApplication* app = MDeclarativeCache::qApplication(argc, argv);
-    app->setProperty("NoMStyle", true);
     QDeclarativeView* view = MDeclarativeCache::qDeclarativeView();
     view->setSource(QUrl("qrc:/qml/main.qml"));
     view->showFullScreen();
 #else
     QApplication *app = new QApplication(argc, argv);
-    app->setProperty("NoMStyle", true);
     QDeclarativeView *view = new QDeclarativeView();
     view->setSource(QUrl("qrc:/qml/main.qml"));
     view->show();
 #endif
 
+    view->setWindowTitle("Flickr");
     QObject::connect(view->engine(), SIGNAL(quit()), view, SLOT(close()));
 
     ImageSaver imageSaver;
